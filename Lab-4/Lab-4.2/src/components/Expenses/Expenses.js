02f104ce5d0204852bc075b61caa779b2f4a42b4
HP Nguyen <phucvovinam97@gmail.com>
2023-10-18 15:16:59 +0700

    Lab 4.2

---
+++ Lab-4/Lab-4.2/src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+
+const Expenses = ({items}) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          filteredYear={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses}/>
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
