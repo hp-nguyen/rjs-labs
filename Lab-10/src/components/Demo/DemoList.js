118d9949eaae0c032ddeb61b3137a53f66049277
HP Nguyen <phucvovinam97@gmail.com>
2023-10-23 23:15:19 +0700

    Init Lab 10.1

---
+++ Lab-10/src/components/Demo/DemoList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import classes from './DemoList.module.css';
+
+const DemoList = (props) => {
+  const { items } = props;
+
+  console.log('Items sorted');
+  const sortedList = items.sort((a, b) => a - b);
+
+  console.log('DemoList RUNNING');
+  return (
+    <div className={classes.list}>
+      <h2>{props.title}</h2>
+      <ul>
+        {sortedList.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DemoList;
