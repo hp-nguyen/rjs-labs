a8fde8ba4f0856dc33134d76bc9e0324b2a086d5
HP Nguyen <phucvovinam97@gmail.com>
2023-11-10 19:32:15 +0700

    Add feedback after ordered

--- Lab-14/Lab-14.2/src/components/Cart/Cart.jsx
+++ Lab-14/Lab-14.2/src/components/Cart/Cart.jsx
@@ -1,23 +1,26 @@
 import { useContext, useState } from 'react';
 
-import Modal from '../UI/Modal';
-import CartItem from './CartItem';
-import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
+import CartItem from './CartItem';
 import CheckoutForm from './CheckoutForm';
 
-const Cart = (props) => {
-  const [isCheckout, setIsCheckout] = useState(false);
+const Cart = props => {
+  const [isCheckout, setIsCheckout] = useState(false);  // Kiểm tra user ấn order chưa
+  const [isSubmitted, setIsSubmitted] = useState(false);  // Kiểm tra request order lên server chưa
+  const [isSuccess, setIsSuccess] = useState(true); // Kiểm tra trạng thái request
+
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {
+  const cartItemRemoveHandler = id => {
     cartCtx.removeItem(id);
   };
 
-  const cartItemAddHandler = (item) => {
+  const cartItemAddHandler = item => {
     cartCtx.addItem({ ...item, amount: 1 });
   };
 
@@ -25,19 +28,34 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
-  const submitOrderHandler = (userData) => {
-    fetch('https://my-test-696e5-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json', {
-      method: 'POST',
-      body: JSON.stringify({
-        user: userData,
-        orderedItems: cartCtx.items
-      })
-    });
+  const submitOrderHandler = async userData => {
+    try {
+      const res = await fetch(
+        'https://my-test-696e5-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            user: userData,
+            orderedItems: cartCtx.items,
+          }),
+        }
+      );
+      if (!res.ok) {
+        throw new Error('Request failed');
+      }
+      // Reset cart nếu đã order thành công
+      cartCtx.resetCart();
+    } catch (error) {
+      setIsSuccess(false);
+      console.error(error.message)
+    }
+
+    setIsSubmitted(true);
   };
 
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {cartCtx.items.map((item) => (
+      {cartCtx.items.map(item => (
         <CartItem
           key={item.id}
           name={item.name}
@@ -62,9 +80,8 @@ const Cart = (props) => {
       )}
     </div>
   );
-
-  return (
-    <Modal onClose={props.onClose}>
+  const CartContent = (
+    <>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -74,6 +91,25 @@ const Cart = (props) => {
         <CheckoutForm onConfirm={submitOrderHandler} onCancel={props.onClose} />
       )}
       {!isCheckout && modalActions}
+    </>
+  );
+  const SubmittedContent = (
+    <>
+      {isSuccess ? (
+        <p>Ordered successfully 😊😊😊</p>
+      ) : (
+        <p>Something went wrong ...</p>
+      )}
+      <div className={classes.actions}>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
+      </div>
+    </>
+  );
+  return (
+    <Modal onClose={props.onClose}>
+      {isSubmitted ? SubmittedContent : CartContent}
     </Modal>
   );
 };
